b6267f736af78dc5e694d12bd85299c1108b1c76
heyin <heyin.cjy@alibaba-inc.com>
2021-05-05 20:29:55 +0800

    ..

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 set -e
 cd /build
+echo > /usr/local/cargo/config << EOF
+[source.crates-io]
+registry = "https://github.com/rust-lang/crates.io-index"
+replace-with = 'ustc'
+[source.ustc]
+registry = "git://mirrors.ustc.edu.cn/crates.io-index"
+EOF
 cargo build --release
 cargo install --path .
 cargo clean
